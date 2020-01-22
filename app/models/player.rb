0de304a9f02eb9c1fe434d8b554613a803ba513c
github username <github email address>
2020-01-22 09:10:41 -0500

    Day 1 Changes/Work p2

--- app/models/player.rb
+++ app/models/player.rb
@@ -29,6 +29,7 @@ class Player < ActiveRecord::Base
         url = "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/#{index}?api_key=#{ENV["API_KEY"]}"
         data = JSON.parse(RestClient.get(url))
         puts "Career Wins: #{data[0]["wins"]}, Losses: #{data[0]["losses"]}, Winrate: #{((data[0]["losses"].to_f)/data[0]["wins"] * 100).round(2)}%"
+        puts 
     end
 end
 
